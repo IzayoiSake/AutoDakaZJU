79a9fb1129eef8924da835536815db89ddbe197a
Izayoi Sake <izayoisakes@gmail.com>
2022-11-13 12:50:26 +0800

    测试cookie

--- daka.py
+++ daka.py
@@ -85,6 +85,7 @@ class AutoDaka:
             if self.cookie != "":
                 print("使用cookie登录")
                 self.Reminder("使用cookie登录")
+                self.Reminder(self.cookie)
                 driver.delete_all_cookies()
                 for line in self.cookie.split(';'):
                     name, value = line.strip().split('=', 1)
